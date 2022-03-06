3530d06047da1be72980c5da231e66f788a0a176
Leonard-ld <1259535860@qq.com>
2022-03-06 15:43:02 +0800

    实现公告板

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,32 @@
 <template>
 <div>
   <div class="box">
-      🔔 {{billboard}}
+      🔔 {{billboard.content}}
   </div>
 </div>
 </template>
 
 <script>
-
+import {getBillboard} from "@/api/billboard"
 export default {
   name: 'HomeView',
   data(){
     return{
-      billboard: "billboard"
+      billboard: {
+        content: ''
+      }
     }
-  }
-}
+  },
+  created() {
+    this.fetchBillboard()
+  },
+  methods: {
+    async fetchBillboard() {
+      getBillboard().then((value) => {
+        const { data } = value;
+        this.billboard = data;
+      });
+    },
+  },
+};
 </script>
