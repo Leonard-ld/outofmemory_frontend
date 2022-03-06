ec9f3fb440f72b2fcbf95b30aee3e708e6b44f0e
Leonard-ld <1259535860@qq.com>
2022-03-06 14:35:49 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// Buefy
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+// ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 
 new Vue({
